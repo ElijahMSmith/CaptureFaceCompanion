ac5d6dc6df3561469b90dd07969a84426d98287d
Eli Smith <mcditto99@gmail.com>
2020-02-03 16:42:23 -0500

    Connection progress

--- app/src/main/java/eli/wearlab/capturefacecompanion/CaptureReceiver.java
+++ app/src/main/java/eli/wearlab/capturefacecompanion/CaptureReceiver.java
@@ -1,45 +1,98 @@
 package eli.wearlab.capturefacecompanion;
 
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.vuzix.connectivity.sdk.Connectivity;
 
 import static android.app.Activity.RESULT_OK;
 
 public class CaptureReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (isOrderedBroadcast() && Connectivity.get(context).verify(intent)) {
+            Log.d("Debug", "Transmission received");
             setResultCode(RESULT_OK);
             setResultData("RECEIVED");
 
-            /*Bundle extras = new Bundle();
-            extras.putString("key", "value");
-            setResultExtras(extras);*/
-
             sendNotification(context, "CaptureFaceCompanion", "Received broadcast from Vuzix!");
+            byte[] imageData = intent.getByteArrayExtra("data");
+            MainActivity.updateImageData(imageData);
+
+            if(MainActivity.isRunning){
+                //Send broadcast from here
+                Intent i = new Intent();
+                i.setAction(MainActivity.CAPTURE_FACE_FILTER);
+                context.sendBroadcast(i);
+            }
+
+            Bundle extras = new Bundle();
+            extras.putString("toast", "WE'VE DONE IT");
+            setResultExtras(extras);
+
+            //NEED TO WORK ON HOW THIS SHOULD GET SET UP TO BEST FUNCTION AS I WANT.
+
+
+            /*File photoFile;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("Debug", "Couldn't create file!");
+                return;
+            }
+            //write the bytes in file
+            FileOutputStream fo;
+            try {
+                fo = new FileOutputStream(photoFile);
+                fo.write(byteData);
+                fo.close();
+            } catch (Exception e) {
+                Log.e("Debug", "FILE IO EXCEPTION");
+                return;
+            }*/
         }
     }
 
+    //For now just temporary, could be used down the line for real purposes
     private void sendNotification(Context context, String name, String description){
         long[] vibrate = new long[2];
         vibrate[0] = 500;
         vibrate[1] = 1000;
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, MainActivity.CHANNELID)
+                .setContentTitle(name)
                 .setContentText(description)
-                .setSmallIcon(R.drawable.ic_received)
+                .setContentText(description)
                 .setLights(Color.GREEN, 500, 2000)
                 .setAutoCancel(true)
                 .setVibrate(vibrate)
-                .setContentTitle(name);
-        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(1, builder.build());
+                .setContentTitle(name)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(1, builder.build());
     }
+
+    /*@SuppressLint("SimpleDateFormat")
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+
+        //Save a file: path for use with ACTION_VIEW intents
+        return File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+    }*/
 }
--- app/src/main/java/eli/wearlab/capturefacecompanion/MainActivity.java
+++ app/src/main/java/eli/wearlab/capturefacecompanion/MainActivity.java
@@ -2,20 +2,72 @@ package eli.wearlab.capturefacecompanion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.AudioAttributes;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.vuzix.connectivity.sdk.Connectivity;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    protected static final String CHANNELID = "CAPTUREFACECOMPANION";
+    protected static final String CAPTURE_FACE_FILTER = "eli.wearlab.captureface.SEND_IMAGE";
+    private static byte[] lastData;
+    protected static boolean isRunning = false;
+
+    private BroadcastReceiver commandReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Debug", "Received communication from broadcast receiver!");
+            Toast.makeText(getApplicationContext(), "Received intent in main!", Toast.LENGTH_SHORT).show();
+            updateImageView();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerReceiver(commandReceiver, new IntentFilter(CAPTURE_FACE_FILTER));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelName = "CaptureFace Companion Notification Channel";
+            String channelDescription = "Notification channel for CaptureFace Companion";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNELID, channelName, importance);
+            channel.setDescription(channelDescription);
+
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if(notificationManager != null)
+                notificationManager.createNotificationChannel(channel);
+        }
+
+        isRunning = true;
+        updateImageView();
 
         /*Set up activity to work with DB Helper
                Any inputs with empty descriptions/names, bring to user the image and let them either change/add the name/info of that person
@@ -28,9 +80,53 @@ public class MainActivity extends AppCompatActivity {
                     Do try to show picture of person next to the name though
                         Use first picture provided
 
-
         */
 
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        isRunning = false;
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        isRunning = true;
+        updateImageView();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        isRunning = false;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        isRunning = true;
+        updateImageView();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        isRunning = false;
+    }
+
+    protected static void updateImageData(byte[] imageData){
+        lastData = imageData;
+    }
+
+    private void updateImageView(){
+        if(lastData == null)
+            return;
+        ImageView lastImage = findViewById(R.id.lastImage);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(lastData, 0, lastData.length);
+        lastImage.setImageBitmap(bitmap);
+        lastImage.setVisibility(View.VISIBLE);
     }
 
 }
